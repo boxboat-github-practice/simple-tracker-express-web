8176896a3303c7fee0cb35138a96aea6e5752253
Mike McMullen <mbmcmullen27@gmail.com>
2023-01-17 16:00:39 -0600

    inital commit

---
+++ sample.js
@@ -0,0 +1,43 @@
+exports.data = {
+  "employees": [
+    {
+      "name": "Mike",
+      "contract": 0,
+      "github": "mbmcmullen27",
+      "id": 1,
+      "history": [
+        {id: 1, name: "delta", role: "engineer"},
+        {id: 2, name: "securegive", role: "engineer"},
+        {id: 3, name: "cca", role: "engineer"}
+      ]
+    },
+    {
+      "name": "Dan",
+      "contract": "none",
+      "github": "dkoch84",
+      "id": 2,
+      "history": [
+        {id: 4, name: "ecolab", role: "architect"},
+        {id: 5, name: "evergy", role: "architect"},
+        {id: 6, name: "travelport", role: "architect"}
+      ]
+    }
+  ],
+
+  "contracts": [
+    {
+      "id": 1,
+      "name": "delta",
+      "type": "app migration",
+      "startDate": "9/2/2022",
+      "endDate": "12/14/2022",
+      "tech": [
+        "openshift",
+        "aws",
+        "cloudformation",
+        "argocd",
+        "jboss"
+      ]
+    }
+  ]
+}
---
+++ server.js
@@ -0,0 +1,49 @@
+
+
+const {data} = require("./sample.js");
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 8080;
+const host = process.env.HOST || '0.0.0.0';
+
+console.log(data)
+
+app.use(cors())
+
+app.set('view engine', 'ejs');
+
+// views
+app.get('/', function(req, res) {
+  res.render("index");
+});
+
+app.get('/profile/:id', async function(req,res) {
+  res.render("user", {id: req.params.id})
+})
+
+app.get('/client/:id', async function(req,res) {
+  res.render("client", {id: req.params.id})
+})
+
+// data
+app.get('/employees', async function(req,res) {
+  res.send(data.employees)
+})
+
+app.get('/employees/:id', async function(req,res) {
+  res.send(data.employees.filter(e=>e.id == req.params.id)[0])
+})
+
+app.get('/contracts', async function(req, res) {
+  res.send(data.contracts)
+})
+
+app.get('/contracts/:id', async function(req,res) {
+  res.send(data.contracts.filter(e=>e.id == req.params.id)[0])
+})
+
+app.listen(port, host);
+console.log(`Server started at http://${host}:${port}`);
